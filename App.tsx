71302310e1dc1ee1030093d94f0617698e2d9efb
Manav Bhanot <manavb@amazon.com>
2022-09-21 10:31:58 -0700

    Adding background image to GameScreen. Flexing and Styling image

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -10,15 +11,34 @@
 
 
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ImageBackground} from 'react-native';
+// import {LinearGradient} from 'react-native-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+        style={styles.imageBackgroundView}>
+        <StartGameScreen />
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  rootScreen: {
+    backgroundColor: '#ddb52f',
+    flex: 1,
+  },
+  imageBackgroundView: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
 });
