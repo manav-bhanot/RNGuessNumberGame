1efd0c7149646cf3223484a9c6d3b1d6552997c7
Manav Bhanot <manavb@amazon.com>
2022-09-20 17:29:21 -0700

    Add styling to buttons

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -2,29 +2,45 @@ import React from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 function PrimaryButton(props) {
-
   function pressHandler() {
-    console.log("Button Presed");
+    console.log('Button Presed');
   }
 
   return (
-    <Pressable onPress={pressHandler}>
-      <View style={styles.container}>
-        <Text>{props.children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={pressData =>
+          pressData.pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{color: 'white'}}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
   );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#72063c',
+  buttonOuterContainer: {
     borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden'
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
     paddingVertical: 8,
     paddingHorizontal: 16,
-    margin: 4,
     elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  pressed: {
+    opacity: 0.75, // opacity takes in values between 0 and 1. 0 => 0% opaque and 100% transparent and 1 => 100% opaque and 0% transparent
   }
 })
