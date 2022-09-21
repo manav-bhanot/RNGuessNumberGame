a15a1ae8d3483610ec532491678dde10d13d10d2
Manav Bhanot <manavb@amazon.com>
2022-09-20 17:07:55 -0700

    Add checks on TextInput to allow only numeric values and also open numeric keyboard

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -6,7 +6,13 @@ function StartGameScreen() {
 
   return (
     <View style={styles.inputContainer}>
-      <TextInput></TextInput>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -17,11 +23,31 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: '#72063c',
     borderRadius: 8,
+    elevation: 4, // ANDROID ONLY property to add shadow behind the component on which this style is applied.
+    
+    // IOS only properties of adding shadow behind a component
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
+  },
+  numberInput: {
+    height: 50,
+    width: '100%',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 })
