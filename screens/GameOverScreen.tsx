4f97df570c0f98d3d84062c5805e159fbfb4cba4
Manav Bhanot <manavb@amazon.com>
2022-09-22 13:15:15 -0700

    Add Start New Game button to game over screen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -3,19 +3,26 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
+import Colors from '../constants/Colors';
 
 function GameOverScreen() {
   return (
     <View style={styles.rootContainer}>
       <Title>GAME OVER !!!</Title>
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={require('../assets/images/gameOver.png')}></Image>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/gameOver.png')}></Image>
       </View>
       <View>
-        <Text>Moves summary</Text>
-        <Text>Your phone needed X rounds to guess the number Y</Text>
+        <Text style={styles.summaryText}>
+          Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+          guess the number <Text style={styles.highlight}>Y</Text>
+        </Text>
       </View>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -41,6 +48,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontWeight: 'bold',
+    color: Colors.primary500,
   },
 });
 
